6a14b471631cd3edddd13285deb4ae9ecacd630b
Asen Sanjula <asensanjula@gmail.com>
2021-07-24 00:24:30 +0530

    info: dynamic page titles using next Head

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import Head from "next/head";
 
 function About(props) {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti explicabo maiores obcaecati odio.
-                Dolores est eveniet expedita fugit maiores? A aperiam assumenda aut autem cumque dolore dolorum fugiat
-                hic magni molestias necessitatibus nobis odio provident quidem, suscipit velit voluptate. Expedita
-                officia, quisquam saepe tempora temporibus voluptas. Magnam repellendus tempore voluptatum?</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About Us</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti explicabo maiores obcaecati odio.
+                    Dolores est eveniet expedita fugit maiores? A aperiam assumenda aut autem cumque dolore dolorum
+                    fugiat
+                    hic magni molestias necessitatibus nobis odio provident quidem, suscipit velit voluptate. Expedita
+                    officia, quisquam saepe tempora temporibus voluptas. Magnam repellendus tempore voluptatum?</p>
+            </div>
+        </>
     );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 export default function Home() {
     return (
-        <div>
-            <h1>HomePage</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto at beatae dicta, dolore
-                dolorum eius eligendi esse fugit, hic minima nisi officia placeat quas soluta temporibus tenetur, vitae
-                voluptas.</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | Home</title>
+            </Head>
+            <div>
+                <h1>HomePage</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto at beatae dicta, dolore
+                    dolorum eius eligendi esse fugit, hic minima nisi officia placeat quas soluta temporibus tenetur,
+                    vitae
+                    voluptas.</p>
+            </div>
+        </>
     )
 }
