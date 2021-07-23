92762e8314141ece03a774180e9aea890bbe7def
Asen Sanjula <asensanjula@gmail.com>
2021-07-23 23:59:18 +0530

    info: add global css and css modules

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer">
+        <footer>
             <h4>Footer</h4>
-        </div>
+        </footer>
     );
 };
 
