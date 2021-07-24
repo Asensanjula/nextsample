29c8f32d8ce60a2fb759dedb942896a353b384ca
Asen Sanjula <asensanjula@gmail.com>
2021-07-24 11:36:40 +0530

    info: static data fetching using getStaticProps method

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import styles from '../../styles/Ninjas.module.css'
 
-function Index(props) {
+export const getStaticProps = async () => {
+
+    console.log("fetching");
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: {ninjas: data}
+    }
+}
+
+function Index({ninjas}) {
+    console.log(ninjas);
     return (
         <div>
             <h1>All Ninjas</h1>
+            {
+                ninjas.map(ninja => (
+                    <div key={ninja.id}>
+                        <a className={styles.single}>
+                            <h3>{ninja.name}</h3>
+                        </a>
+                    </div>
+                ))
+            }
         </div>
     );
 }
