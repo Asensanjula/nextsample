44a5c22761b26ff3af44d2a80f92a13b07f6de17
Asen Sanjula <asensanjula@gmail.com>
2021-09-01 12:58:53 +0530

    feat: react scroll to top

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import TryMobileBanner from "./TryMobileBanner";
+import ScrollToTop from "react-scroll-to-top";
 
 const Layout = ({children}) => {
     return (
         <div className="content">
             <TryMobileBanner/>
             <Navbar/>
+            <ScrollToTop smooth />
             {children}
             <Footer/>
         </div>
